6b514f6e3068ad80b5ebb13656a7fdd632ca36d3
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-30 14:16:42 -0700

    breaker header into seperate children containers

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,29 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
+import "../css/Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h1>I am the header</h1>
-      <MenuIcon />
-      <img
-        className="header__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-        alt=""
-      />
-      <input type="text" />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
-      <Avatar />
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+          alt=""
+        />
+      </div>
+      <div className="header__input">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar alt="" src="" />
+      </div>
     </div>
   );
 }
