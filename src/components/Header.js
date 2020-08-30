d06ca8aabde8f6422d9ce76da6fbe799a911194e
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-30 14:08:29 -0700

    added Notificiations Icon and Avatar icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+
 function Header() {
   return (
     <div className="header">
@@ -17,6 +20,8 @@ function Header() {
       <SearchIcon />
       <VideoCallIcon />
       <AppsIcon />
+      <NotificationsIcon />
+      <Avatar />
     </div>
   );
 }
