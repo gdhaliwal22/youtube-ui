53c9c372d04a81e211234f11041f79746c5feab4
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-30 14:06:53 -0700

    added SearchIcon, VideoCallIcon, and AppIcon components to Header.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
       {/* Header */}
       <Header />
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
 function Header() {
   return (
     <div className="header">
       <h1>I am the header</h1>
+      <MenuIcon />
+      <img
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+        alt=""
+      />
+      <input type="text" />
+      <SearchIcon />
+      <VideoCallIcon />
+      <AppsIcon />
     </div>
   );
 }
