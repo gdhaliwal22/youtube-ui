cca1aa12366fcefd349981a8587cdbd420d36561
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 12:12:02 -0700

    added more icons for the sidebar and menu options

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,12 @@ import SidebarRow from "../components/SidebarRow";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import HomeIcon from "@material-ui/icons/Home";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
 
 function Sidebar() {
   return (
@@ -11,6 +17,14 @@ function Sidebar() {
       <SidebarRow Icon={HomeIcon} title="Home" selected />
       <SidebarRow Icon={WhatshotIcon} title="Trending" />
       <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked Videos" />
+      <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show More" />
+      <hr />
     </div>
   );
 }
