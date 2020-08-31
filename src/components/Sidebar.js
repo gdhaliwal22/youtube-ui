61e252d5800f7c62f2fd3c740295cef2d6c81bfb
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 11:33:48 -0700

    created SidebarRow component, pass title props

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "../css/Sidebar.css";
+import SidebarRow from "../components/SidebarRow";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <h1>I am Sidebar </h1>
+      <SidebarRow title="Home" />
+      <SidebarRow title="Trending" />
+      <SidebarRow title="Subscription" />
+      {/* <SidebarRow title="" />
+      <SidebarRow title="" />
+      <SidebarRow title="" /> */}
     </div>
   );
 }
