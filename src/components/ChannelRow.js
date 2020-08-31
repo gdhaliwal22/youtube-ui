ccc844fbb67112f967896d9c48c813aa3243f3c9
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 14:00:58 -0700

    create ChannelRow component

---
+++ src/components/ChannelRow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChannelRow() {
+  return (
+    <div>
+      <h1>Channel Row</h1>
+    </div>
+  );
+}
+
+export default ChannelRow;
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
+import ChannelRow from "../components/ChannelRow";
+// import VideoRow from "../components/VideoRow";
 import "../css/SearchPage.css";
 
 function SearchPage() {
@@ -9,6 +11,20 @@ function SearchPage() {
         <TuneOutlinedIcon />
         <h2>FILTER</h2>
       </div>
+      <hr />
+      <ChannelRow
+        image={
+          "https://yt3.ggpht.com/a/AATXAJzNvwShqAXWgyOy3UWrum4dAF49r5jH9PrrBTPiRg=s144-c-k-c0xffffffff-no-rj-mo"
+        }
+        channelName={"youness"}
+        verified
+        subs={"234K"}
+        noOfVideos={300}
+        description={
+          "Don't forget to subscribe for more covers and tutorials !"
+        }
+      />
+      <hr />
     </div>
   );
 }
