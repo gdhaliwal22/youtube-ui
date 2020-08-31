bfdaa50fb2c2248e2e02b7746399319ea3fbba63
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 12:52:12 -0700

    added react router dependency

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -8,159 +8,84 @@ function RecommendedVideos() {
       <h2>Recommended</h2>
       <div className="recommended__videos">
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/122550486/original/edee05c8fa12e475a81fdfea84307c1d667ae192/design-cathy-youtube-facebook-thumbnail-cover-image.jpg"
-          }
-          timestamp={"2 days ago"}
-          views={"2.3M Views"}
-          channel={"Le Monde"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQolgbJM3mV3FEtYJF8OQxL28EG894GWkRW0A&usqp=CAU"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBiSCrWOo4xSWC7PtRiqD5WKcGVpg"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129593745/original/e1f3e1fd770f5cd89117ef8908965a56bdc13a2a/design-attractive-youtube-video-thumbnail.png"
-          }
-          timestamp={"3 weeks ago"}
-          views={"23.3M Views"}
-          channel={"Pranks"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="Whether we like it or not..."
+          views="2M Views"
+          timestamp="4 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GjNWIVnoFbGeEFPRPPfBGGAl6Whxl8dXXsmyA=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Christian Guzman"
+          image="https://i.ytimg.com/vi/5JGlr-FVKsk/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDpqEdKba2dusPvXa6kUZBc914M4Q"
         />
-
         <VideoCard
-          title={"BlackMailed !!!!!!!!!!!!!!!"}
-          image={
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXA4cG1lKHMf1ZKgEo3dkMYQoWKm34qBRBzg&usqp=CAU"
-          }
-          timestamp={"12 days ago"}
-          views={"40.3M Views"}
-          channel={"PUBG"}
-          channelImage={
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRvuhWrrycBEaBcTvH_7IYTc5d0Mjh4T4naQ&usqp=CAU"
-          }
+          title="5 Uses for cloud functions"
+          views="850K Views"
+          timestamp="1 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Gg-7xgPV76_c0nFmsYJLGUTUkJCrPWcnyULag=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Firebase"
+          image="https://i.ytimg.com/vi/77XmRDtOL7c/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDIiXYbD6TYhgtfWFv2qvFQ8WgQLA"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/122550486/original/edee05c8fa12e475a81fdfea84307c1d667ae192/design-cathy-youtube-facebook-thumbnail-cover-image.jpg"
-          }
-          timestamp={"2 days ago"}
-          views={"2.3M Views"}
-          channel={"Le Monde"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="The Truth about OnePlus Nord!"
+          views="5M Views"
+          timestamp="58 Minutes ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Ghl8U3OeJOtu1LRmTNuW7Aiq-iKaU5U6gYWoA=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Marques Brownlee"
+          image="https://i.ytimg.com/vi/Xzh8BdaaAvs/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBFVU97RnCsjtboTaR3TQzvoosgWg"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/122550486/original/edee05c8fa12e475a81fdfea84307c1d667ae192/design-cathy-youtube-facebook-thumbnail-cover-image.jpg"
-          }
-          timestamp={"2 days ago"}
-          views={"2.3M Views"}
-          channel={"Le Monde"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="Elon Musk says Teslas are too expensive!"
+          views="1.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/vi/ZMsDib3wfuY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB9Wei5mn2iCrSIFFQCRfarGe6gBA"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/ZMsDib3wfuY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB9Wei5mn2iCrSIFFQCRfarGe6gBA"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129593745/original/e1f3e1fd770f5cd89117ef8908965a56bdc13a2a/design-attractive-youtube-video-thumbnail.png"
-          }
-          timestamp={"3 weeks ago"}
-          views={"23.3M Views"}
-          channel={"Pranks"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQolgbJM3mV3FEtYJF8OQxL28EG894GWkRW0A&usqp=CAU"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBiSCrWOo4xSWC7PtRiqD5WKcGVpg"
         />
-
         <VideoCard
-          title={"BlackMailed !!!!!!!!!!!!!!!"}
-          image={
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXA4cG1lKHMf1ZKgEo3dkMYQoWKm34qBRBzg&usqp=CAU"
-          }
-          timestamp={"12 days ago"}
-          views={"40.3M Views"}
-          channel={"PUBG"}
-          channelImage={
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRvuhWrrycBEaBcTvH_7IYTc5d0Mjh4T4naQ&usqp=CAU"
-          }
+          title="Whether we like it or not..."
+          views="2M Views"
+          timestamp="4 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GjNWIVnoFbGeEFPRPPfBGGAl6Whxl8dXXsmyA=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Christian Guzman"
+          image="https://i.ytimg.com/vi/5JGlr-FVKsk/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDpqEdKba2dusPvXa6kUZBc914M4Q"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/122550486/original/edee05c8fa12e475a81fdfea84307c1d667ae192/design-cathy-youtube-facebook-thumbnail-cover-image.jpg"
-          }
-          timestamp={"2 days ago"}
-          views={"2.3M Views"}
-          channel={"Le Monde"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="5 Uses for cloud functions"
+          views="850K Views"
+          timestamp="1 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Gg-7xgPV76_c0nFmsYJLGUTUkJCrPWcnyULag=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Firebase"
+          image="https://i.ytimg.com/vi/77XmRDtOL7c/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDIiXYbD6TYhgtfWFv2qvFQ8WgQLA"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/122550486/original/edee05c8fa12e475a81fdfea84307c1d667ae192/design-cathy-youtube-facebook-thumbnail-cover-image.jpg"
-          }
-          timestamp={"2 days ago"}
-          views={"2.3M Views"}
-          channel={"Le Monde"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="The Truth about OnePlus Nord!"
+          views="5M Views"
+          timestamp="58 Minutes ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Ghl8U3OeJOtu1LRmTNuW7Aiq-iKaU5U6gYWoA=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Marques Brownlee"
+          image="https://i.ytimg.com/vi/Xzh8BdaaAvs/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBFVU97RnCsjtboTaR3TQzvoosgWg"
         />
-
         <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129593745/original/e1f3e1fd770f5cd89117ef8908965a56bdc13a2a/design-attractive-youtube-video-thumbnail.png"
-          }
-          timestamp={"3 weeks ago"}
-          views={"23.3M Views"}
-          channel={"Pranks"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
-        />
-
-        <VideoCard
-          title={"BlackMailed !!!!!!!!!!!!!!!"}
-          image={
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXA4cG1lKHMf1ZKgEo3dkMYQoWKm34qBRBzg&usqp=CAU"
-          }
-          timestamp={"12 days ago"}
-          views={"40.3M Views"}
-          channel={"PUBG"}
-          channelImage={
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRvuhWrrycBEaBcTvH_7IYTc5d0Mjh4T4naQ&usqp=CAU"
-          }
-        />
-
-        <VideoCard
-          title={"This is a Title about animals"}
-          image={
-            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/122550486/original/edee05c8fa12e475a81fdfea84307c1d667ae192/design-cathy-youtube-facebook-thumbnail-cover-image.jpg"
-          }
-          timestamp={"2 days ago"}
-          views={"2.3M Views"}
-          channel={"Le Monde"}
-          channelImage={
-            "https://media-cdn.tripadvisor.com/media/photo-s/06/d4/ae/7c/crystal-rock.jpg"
-          }
+          title="Elon Musk says Teslas are too expensive!"
+          views="1.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/vi/ZMsDib3wfuY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB9Wei5mn2iCrSIFFQCRfarGe6gBA"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/ZMsDib3wfuY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB9Wei5mn2iCrSIFFQCRfarGe6gBA"
         />
       </div>
     </div>
