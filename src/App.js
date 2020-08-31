9858969ef1c668056521ff45309ee976c8a5cd7b
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 13:01:35 -0700

    added wildcard for searchTerm in Router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import RecommendedVideos from "./components/RecommendedVideos";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./css/App.css";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+          <Route path="/search/:searchTerm">
+            <Header />
+            <h1>Search</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
