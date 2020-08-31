8d44e3834a55b7e7299dc6b9402dd5f27fb5ac22
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 11:29:26 -0700

    created recommended video component

---
+++ src/components/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/RecommendedVideos.css";
+
+function RecommendedVideos() {
+  return (
+    <div className="recommendedVideos">
+      <h1>I am Videos section</h1>
+    </div>
+  );
+}
+
+export default RecommendedVideos;
