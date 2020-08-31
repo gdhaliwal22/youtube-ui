8637029ac9208292b2e3ea65d420b882129dd041
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-31 15:13:01 -0700

    fixed highlighting on subscribers in videoRow

--- src/components/VideoRow.js
+++ src/components/VideoRow.js
@@ -17,13 +17,12 @@ function VideoRow({
         <h3>{title}</h3>
         <p className="videoRow__headline">
           {channel} •
-          <span className="videoRow__text__subs">
-            <span className="videoRow__text__subsNumber">{subs}</span>
-            Subscribers
-          </span>
-          • {views} Views • {timestamp}
+          <span className="videoRow__subs">
+            <span className="videoRow__subsNumber"> {subs} subscribers</span>
+          </span>{" "}
+          {views} views • {timestamp}
         </p>
-        <p>{description}</p>
+        <p className="videoRow__description">{description}</p>
       </div>
     </div>
   );
